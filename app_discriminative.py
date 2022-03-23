8fb80b471a65472299e6af05f115fe39c9573b23
MarinkaPo <polukoshko.marina@gmail.com>
2022-03-23 21:35:27 +0300

    23.03.22

--- app_discriminative.py
+++ app_discriminative.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import streamlit as st
 
+import torch,torchvision
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
